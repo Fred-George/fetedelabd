7715c29883a9be3d3cf4cee6977bc3d1864af83c
Fred-George <f.george@visit.brussels>
2019-05-17 14:01:23 +0200

    first commit

---
+++ web/themes/custom/visit/js/visit.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.exampleBehavior = {
+    attach: function (context, settings) {
+      $(document).ready(function () {
+
+      });
+      
+      
+      /* Preloader and animations */
+      
+      $(window).on('load', function () { // makes sure the whole site is loaded
+        $('#status').fadeOut(); // will first fade out the loading animation
+        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({'overflow-y': 'visible'});
+      });
+    }
+  };
+
+})(jQuery, Drupal);
